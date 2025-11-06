8a0360b2090762946afd439dc8a94a4d0b691d8d
Muhammad Talha <i228798@nu.edu.pk>
2025-11-06 15:10:00 +0500

    r

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,8 +1,10 @@
 // src/sum.test.js
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-expect(sum(2, 2)).toBe(4);
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
 });
 
-test("adds 0 + 0 to equal to 0", () => expect(sum(0+0).toBe(0)));
+test('adds 0 + 0 to equal 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
